625210d19ace26e696f17624ea79adc259f7974a
Maha Mohammed <mm.alruqi@tawari.sa>
2020-12-01 13:09:11 +0300

    first commit

---
+++ starter_code/backend/src/auth/auth.py
@@ -0,0 +1,96 @@
+import json
+from flask import request, _request_ctx_stack
+from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+
+AUTH0_DOMAIN = 'dev-fsnd-coffee.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'image'
+
+## AuthError Exception
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+## Auth Header from Auth0 Documentation
+
+def get_token_auth_header():
+    header = request.headers.get("Authorization", None)
+    if not header:
+        raise AuthError({"code": "authorization_header_missing","description":"Authorization header is expected"}, 401)
+
+    parts = header.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header","description":"Authorization header must start with Bearer"}, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header","description": "Token not found"}, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header","description":"Authorization header must be Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+    
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({"code": "invalid","description": "Permissions are not included in the payload"}, 400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({"code": "invalid","description": "Permission not found"}, 402)
+    return True
+
+## verify_decode_jwt from Auth0 Documentation
+def verify_decode_jwt(token):
+    jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer="https://"+AUTH0_DOMAIN+"/"
+            )
+        except jwt.ExpiredSignatureError:
+            raise AuthError({"code": "token_expired","description": "Token is Expired"}, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({"code": "invalid_claims","description":"Incorrect Claims, please check the audience and issuer"}, 401)
+
+        except Exception:
+            raise AuthError({"code": "invalid_header","description":"Unable to parse authentication token."}, 401)
+
+        _request_ctx_stack.top.current_user = payload
+        return f(*args, **kwargs)
+    raise AuthError({"code": "invalid_header","description": "Unable to find appropriate key"}, 401)
+
+    return decorated
+
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            payload = verify_decode_jwt(token)
+            check_permissions(permission, payload)
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
